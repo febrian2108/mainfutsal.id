eb9365907e1f26b983d33c5eca5f89f7f91d8e3c
Fahri <febrian2104@gmail.com>
2025-04-14 12:44:37 +0700

    first commit

---
+++ user-service/constants/error/user.go
@@ -0,0 +1,17 @@
+package error
+
+import "errors"
+
+var (
+	ErrUserNotFound         = errors.New("user not found")
+	ErrPasswordIncorrect    = errors.New("password incorrect")
+	ErrUserAlreadyExists    = errors.New("user already exists")
+	ErrPasswordDoesNotMatch = errors.New("password does not match")
+)
+
+var UserError = []error{
+	ErrUserNotFound,
+	ErrPasswordIncorrect,
+	ErrUserAlreadyExists,
+	ErrPasswordDoesNotMatch,
+}
